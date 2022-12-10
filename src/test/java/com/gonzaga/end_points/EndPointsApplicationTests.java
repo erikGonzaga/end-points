89bc2f80403bff4824891ac9b583828ea093f2aa
Erik Gonzaga <erikgonzaga@outlook.com>
2022-12-10 14:35:52 -0300

    Inicio de Projeto

---
+++ src/test/java/com/gonzaga/end_points/EndPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzaga.end_points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
